bc97e7a8f474416b9f10b1fdfbbaa04951354968
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2025-10-22 16:00:53 -0300

    ajustando rodas e redirects

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from db import db
 from models import Noticia
 
@@ -12,6 +12,15 @@ db.init_app(app)
 def home():
     return render_template("index.html")
 
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/nova_noticia", methods=['GET', 'POST'])
+def nova_noticia():
+    if request.method == 'GET':
+        return render_template("nova-noticia.html")
+        
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
